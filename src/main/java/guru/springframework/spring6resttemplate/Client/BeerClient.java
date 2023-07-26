25edd81b7db47c0b41e00ae8544023aa992bacea
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-26 04:49:31 +0100

    initial project add

--- src/main/java/guru/springframework/spring6resttemplate/Client/BeerClient.java
+++ src/main/java/guru/springframework/spring6resttemplate/Client/BeerClient.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring6resttemplate.Client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import org.springframework.data.domain.Page;
 
 public interface BeerClient {
 
