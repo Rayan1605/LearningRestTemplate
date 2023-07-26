2f2615a6771c9f18614ab647d2b29baf217f2c3e
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-26 05:00:33 +0100

    initial project add

--- src/main/java/guru/springframework/spring6resttemplate/Client/BeerClientImp.java
+++ src/main/java/guru/springframework/spring6resttemplate/Client/BeerClientImp.java
@@ -1,11 +1,27 @@
 package guru.springframework.spring6resttemplate.Client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
 
 public class BeerClientImp implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
     @Override
     public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        ResponseEntity<String> stringResponse = restTemplate.getForEntity(
+                "https://localhost:8080/api/v1/beers", String.class
+        );
+        System.out.println(stringResponse.getBody());
         return null;
     }
 }
