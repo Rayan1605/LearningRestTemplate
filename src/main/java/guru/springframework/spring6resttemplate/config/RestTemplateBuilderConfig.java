ad3aed24b9d7784394384f4fe38ddb0bc58192fe
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-28 16:55:13 +0100

    initial project add

---
+++ src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring6resttemplate.config;
+
+import lombok.Builder;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+public class RestTemplateBuilderConfig {
+    @Bean
+    RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer){
+
+        RestTemplateBuilder restTemplateBuilder =  configurer.configure(new RestTemplateBuilder());
+        DefaultUriBuilderFactory defaultUriBuilderFactory =
+                new DefaultUriBuilderFactory("http://localhost:8080");
+        
+        return restTemplateBuilder.uriTemplateHandler(defaultUriBuilderFactory);
+
+    }
+}
