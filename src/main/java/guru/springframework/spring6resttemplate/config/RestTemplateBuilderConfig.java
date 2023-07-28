2e537b295864102fc25f81c0298d00cf2987ef55
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-28 17:12:41 +0100

    initial project add

--- src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -1,13 +1,16 @@
 package guru.springframework.spring6resttemplate.config;
 
 import lombok.Builder;
+import lombok.Value;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.util.DefaultUriBuilderFactory;
-
+@Configuration
 public class RestTemplateBuilderConfig {
+
   //The RestTemplateBuilder in Spring is used to create and configure RestTemplate instances.
     // Here are some key points:
     //
