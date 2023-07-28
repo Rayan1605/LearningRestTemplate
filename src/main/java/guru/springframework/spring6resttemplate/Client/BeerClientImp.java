c56b4c097676960b78af06610dee1abf2ff9adae
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-28 16:01:55 +0100

    initial project add

--- src/main/java/guru/springframework/spring6resttemplate/Client/BeerClientImp.java
+++ src/main/java/guru/springframework/spring6resttemplate/Client/BeerClientImp.java
@@ -19,7 +19,16 @@ public class BeerClientImp implements BeerClient {
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
+//restTemplate.getForEntity()
+// makes a GET request to the "https://localhost:8080/api/v1/beer" endpoint.
 
+    //The first request specifies String.class as the response type,
+        // so the response body is returned as a String.
+        //The second request specifies Map.class, so the response is deserialized into a
+        // Map.
+        //The ResponseEntity contains both the response body and additional metadata
+        // like status code.
+        //So the first call gets the beer API response as a String, the second as a Map.
         ResponseEntity<String> stringResponse = restTemplate.getForEntity(
                 "https://localhost:8080/api/v1/beer", String.class
         );
