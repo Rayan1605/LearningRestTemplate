b68537a885f75a5be87e1ea318ec40b9f5492b0f
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-28 16:38:35 +0100

    initial project add

--- src/main/java/guru/springframework/spring6resttemplate/model/RestPageImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/model/RestPageImpl.java
@@ -1,4 +1,29 @@
 package guru.springframework.spring6resttemplate.model;
 
-public class RestPageImpl {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class RestPageImpl<T> extends PageImpl<T> {
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public RestPageImpl(@JsonProperty("content") List<T> content,
+                        @JsonProperty("number") int page,
+                        @JsonProperty("size") int size,
+                        @JsonProperty("totalElements") long totalElements) {
+        super(content, PageRequest.of(page, size), totalElements);
+    }
+
+
+    public RestPageImpl(List<T> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public RestPageImpl(List<T> content) {
+        super(content);
+    }
 }
